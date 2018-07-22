23765c95869bf3c9bba3210dcf644fc786a65089
ranshuai <18210702905@163.com>
2018-07-23 00:02:39 +0800

    发布商品

--- pages/free_ride/free_ride.js
+++ pages/free_ride/free_ride.js
@@ -4,14 +4,36 @@ Page({
    * 页面的初始数据
    */
   data: {
-    oneLevelNav:''
+    oneLevelNav:'', //点击的类型
+    releaseList:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    var _this = this;
+    wx.request({
+      url: 'https://riseupall.cn/server/getrelease',
+      data: {
+        releaseType: '0'
+      },
+      success: function (data) {
+        console.log(data);
+        data = data.data;
+        if(data.success){
+          _this.setData({
+            releaseList: data.result.list
+          })
+          console.log(_this.data.releaseList);
+        }else{
+          wx.showToast({
+            title: data.msg,
+            icon: 'none'
+          })
+        }
+      }
+    })
   },
 
   /**
@@ -78,11 +100,38 @@ Page({
   },
   //点击一级导航
   oneLevelNavClick: function (ev) {
+    wx.showLoading({
+      title: '加载中',
+    })
+    var _this = this;
     if (ev.currentTarget.dataset.info == this.data.oneLevelNav) {
       return;
     }
     this.setData({
       oneLevelNav: ev.currentTarget.dataset.info
+    })
+    wx.request({
+      url: 'https://riseupall.cn/server/getrelease',
+      data:{
+        releaseType: this.data.oneLevelNav
+      },
+      success: function (data) {
+        console.log(data);
+        var data = data.data
+        if(data.success){
+          _this.setData({
+            releaseList: data.result.list
+          })
+        }else{
+          wx.showToast({
+            title: data.msg,
+            icon: 'none'
+          })
+        }
+      },
+      complete: function () {
+        wx.hideLoading();
+      }
     })
   },
 })
